7dd4cd42cef86cf188239249e8343f2f986f0a75
rexroy73 <rexroy73@gmail.com>
2018-06-14 13:26:07 +0530

    Spam Detector
    This is a very basic and simple project I began with which is a spam detector using Naive Bayes classifier and Adaboost classifier. The data set used is pre-processed and the features have already been extracted I just loaded the data set and carried out these different classification and compared them to see which gives the highest accuracy.

---
+++ spam_detector.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  6 17:58:51 2018
+
+@author: Prashita
+"""
+
+from sklearn.naive_bayes import MultinomialNB
+
+data= pd.read_csv('spambase.data').as_matrix()
+
+np.random.shuffle(data)
+
+#Dataset
+X= data[:, :48]
+Y= data[:, -1]
+
+#Division of dataset
+Xtrain = X[:-100,]
+Ytrain = Y[:-100,]
+Xtest = X[-100:,]
+Ytest = Y[-100:,]
+
+#fitting the model
+model = MultinomialNB()
+model.fit(Xtrain, Ytrain)
+print ("Classification rate for NB:", model.score(Xtest, Ytest))
+
+############ you can use any model ###########
+from sklearn.ensemble import AdaBoostClassifier
+
+model1 = AdaBoostClassifier()
+model.fit(Xtrain, Ytrain)
+print ("Classification rate for Adaboost:", model.score(Xtest, Ytest))
